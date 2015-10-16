739ba7a7277544847e01e22c25f6f4f40937a330
metroxel <megan.troxel.12@cnu.edu>
2015-10-16 08:07:53 -0400

    Updated test file

--- ReversalTest.java
+++ ReversalTest.java
@@ -21,23 +21,17 @@ public class ReversalTest {
 		writeOut.println(" ");
 		writeOut.flush();
 		writeOut.close();
-		// write to input file
-		PrintWriter write = new PrintWriter(input);
-		write.println("The cat runs");
-		write.println("The dog runs");
-		write.flush();
-		write.close();
 
 		// call Reversal code
 		Reversal.reverseFile(input, output);
 
 		// Compare to see if correct
 		Scanner scan = new Scanner(output);
-		String expected = "runs cat The";
+		String expected = "runs dog The";
 		String actual = scan.nextLine();
 		assertEquals("Output file is not correct", expected, actual);
 
-		String expected2 = "runs dog The";
+		String expected2 = "runs cat The";
 		String actual2 = scan.nextLine();
 		assertEquals("Output file is not correct", expected2, actual2);
 		scan.close();
