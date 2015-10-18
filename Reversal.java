d2d739f7eb1e97e1d8b48b641cca09bdf5e95a46
metroxel <megan.troxel.12@cnu.edu>
2015-10-18 10:34:41 -0400

    Updated code and fixed bugs

--- Reversal.java
+++ Reversal.java
@@ -11,7 +11,7 @@ import java.util.StringTokenizer;
  * Class Reversal
  * 
  * @author Megan Troxel
- * @version 10/15/15
+ * @version 10/16/15
  *
  */
 public class Reversal {
@@ -26,28 +26,34 @@ public class Reversal {
 	public static void reverseFile(File input, File output)
 			throws FileNotFoundException {
 
-		String wordsInReverse = "";
 		String tempForTokens = "";
 		ArrayList<String> linesToReverse = new ArrayList<String>();
 		String temp = "";
 		PrintWriter writeToOut = new PrintWriter(output);
+		Scanner scan = new Scanner(input);
+		StringTokenizer tokenizer;
 
 		try {
-			// reads in data
-			Scanner scan = new Scanner(input);
+			// reads data to then be manipulated
 			while (scan.hasNext()) {
 
 				tempForTokens = scan.nextLine();
 				// create a tokenizer to split words up at spaces
-				StringTokenizer tokenizer = new StringTokenizer(tempForTokens,
-						" ");
+				tokenizer = new StringTokenizer(tempForTokens, " ");
 
-				// while there are still words, add to temp String in reverse
-				// order
-				temp = tokenizer.nextToken();
-				while (tokenizer.hasMoreTokens()) {
-					temp = tokenizer.nextToken() + " " + temp;
+				// check for empty lines
+				if (tempForTokens.equals("")) {
+					temp = "";
+				} else {
+					// add first word to temp String
+					temp = tokenizer.nextToken();
+					// while there are still words, add to temp String in
+					// reverse
+					// order
+					while (tokenizer.hasMoreTokens()) {
+						temp = tokenizer.nextToken() + " " + temp;
 
+					}
 				}
 
 				linesToReverse.add(temp);
@@ -57,20 +63,24 @@ public class Reversal {
 
 			// reverse the arrayList
 			Collections.reverse(linesToReverse);
+
 			// write to output file
 			for (int i = 0; i < linesToReverse.size(); i++) {
 				writeToOut.println(linesToReverse.get(i));
 			}
+
 			writeToOut.flush();
-			writeToOut.close();
 
 			// be sure to close everything since done
+			writeToOut.close();
 			scan.close();
 
-		} catch (FileNotFoundException e) {
-			System.out.print("File does not exist");
-		} catch (IOException e) {
-			System.out.print("File was not read properly");
+		} catch (Exception ex) {
+			if (ex instanceof FileNotFoundException) {
+				System.out.print("File does not exist");
+			} else if (ex instanceof IOException) {
+				System.out.print("File was not read properly");
+			}
 		}
 	}
 }
